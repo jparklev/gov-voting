5d93aa1ecba53a037bbcde689095e379e66b9f71
Joshua Levine <joshuapark95@gmail.com>
2018-08-31 14:18:54 -0700

    retrofit old vote proxy contracts to integrate polling

---
+++ src/vote-proxy.sol
@@ -0,0 +1,62 @@
+// Guy - vote w/ a proxy identity
+
+pragma solidity ^0.4.24;
+
+import "ds-token/token.sol";
+import "ds-chief/chief.sol";
+import "./polling.sol";
+
+contract VoteProxy {
+    address public cold;
+    address public hot;
+    DSToken public gov;
+    DSToken public iou;
+    DSChief public chief;
+    Polling public polling;
+
+    constructor(DSChief _chief, Polling _polling, address _cold, address _hot) public {
+        hot = _hot;
+        cold = _cold;
+        chief = _chief;
+        polling = _polling;
+        
+        gov = chief.GOV();
+        iou = chief.IOU();
+        gov.approve(chief, uint256(-1));
+        iou.approve(chief, uint256(-1));
+        iou.approve(polling, uint256(-1));
+    }
+
+    modifier auth() {
+        require(msg.sender == hot || msg.sender == cold);
+        _;
+    }
+    
+    function lock(uint256 wad) public auth {
+        gov.pull(cold, wad); // mkr from cold 
+        chief.lock(wad);  // mkr out, ious in
+        polling.lock(wad); // ious out
+    }
+
+    function free(uint256 wad) public auth {
+        polling.free(wad); // ious in
+        chief.free(wad);  // ious out, mkr in
+        gov.push(cold, wad); // mkr to cold
+    }
+
+    function voteExec(address[] yays) public auth returns (bytes32 slate) {
+        return chief.vote(yays);
+    }
+
+    function voteExec(bytes32 slate) public auth {
+        chief.vote(slate);
+    }
+
+    function voteGov(uint256 id, bool yay, bytes logData) public auth {
+        polling.vote(id, yay, logData);
+    }
+
+    function retractGov(uint256 id) public auth {
+        polling.unSay(id);
+    }
+}
---
+++ src/vote-proxy.t.sol
@@ -0,0 +1,201 @@
+pragma solidity ^0.4.21;
+
+import "ds-test/test.sol";
+import "ds-token/token.sol";
+import "ds-chief/chief.sol";
+
+import "./vote-proxy.sol";
+import "./polling.sol";
+
+contract Voter {
+    DSToken gov;
+    DSToken iou;
+    DSChief chief;
+    Polling polling;
+    VoteProxy public proxy;
+
+    constructor(DSChief chief_, Polling polling_, DSToken gov_, DSToken iou_) public {
+        iou = iou_;
+        gov = gov_;
+        chief = chief_;
+        polling = polling_;
+    }
+
+    function setProxy(VoteProxy proxy_) public {
+        proxy = proxy_;
+    }
+
+    function doChiefLock(uint amt) public {
+        chief.lock(amt);
+    }
+
+    function doChiefFree(uint amt) public {
+        chief.free(amt);
+    }
+
+    function approveGov(address guy) public {
+        gov.approve(guy);
+    }
+
+    function approveIou(address guy) public {
+        iou.approve(guy);
+    }
+
+    function doProxyLock(uint amt) public {
+        proxy.lock(amt);
+    }
+
+    function doProxyFree(uint amt) public {
+        proxy.free(amt);
+    }
+
+    function doProxyVoteExec(address[] yays) public returns (bytes32 slate) {
+        return proxy.voteExec(yays);
+    }
+
+    function doProxyVoteExec(bytes32 slate) public {
+        proxy.voteExec(slate);
+    }
+
+    function doProxyVoteGov(uint256 id, bool yay, bytes logData) public {
+        proxy.voteGov(id, yay, logData);
+    }
+}
+
+contract WarpPolling is Polling {
+    uint48 _era; uint32 _age;
+    function warp(uint48 era_, uint32 age_) public { _era = era_; _age = age_; }
+    function era() public view returns (uint48) { return _era; } 
+    function age() public view returns (uint32) { return _age; }       
+    constructor(DSToken _gov) public Polling(_gov) {}
+}
+
+contract VoteProxyTest is DSTest {
+    uint256 constant electionSize = 3;
+    address constant c1 = 0x1;
+    address constant c2 = 0x2;
+    bytes byts;
+
+    VoteProxy proxy;
+    DSToken gov;
+    DSToken iou;
+    DSChief chief;
+    WarpPolling polling;
+
+    Voter cold;
+    Voter hot;
+    Voter random;
+
+    function setUp() public {
+        gov = new DSToken("GOV");
+
+        DSChiefFab fab = new DSChiefFab();
+        chief = fab.newChief(gov, electionSize);
+        iou = chief.IOU();
+        polling = new WarpPolling(iou);
+        polling.warp(1 hours, 1);
+
+        cold = new Voter(chief, polling, gov, iou);
+        hot = new Voter(chief, polling, gov, iou);
+        random = new Voter(chief, polling, gov, iou);
+        gov.mint(cold, 100 ether);
+
+        proxy = new VoteProxy(chief, polling, cold, hot);
+
+        random.setProxy(proxy);
+        cold.setProxy(proxy);
+        hot.setProxy(proxy);
+    }
+
+    // sainity test -> cold can lock and free gov tokens with chief directly
+    function test_chief_lock_free() public {
+        cold.approveGov(chief);
+        cold.approveIou(chief);
+
+        cold.doChiefLock(100 ether);
+        assertEq(gov.balanceOf(cold), 0);
+        assertEq(gov.balanceOf(chief), 100 ether);
+
+        cold.doChiefFree(100 ether);
+        assertEq(gov.balanceOf(cold), 100 ether);
+        assertEq(gov.balanceOf(chief), 0 ether);
+    }
+
+    function test_cold_lock_free() public {
+        cold.approveGov(proxy);
+        assertEq(gov.balanceOf(cold), 100 ether);
+        assertEq(iou.balanceOf(polling), 0 ether);
+        assertEq(gov.balanceOf(chief), 0 ether);
+
+        cold.doProxyLock(100 ether);
+        assertEq(gov.balanceOf(cold), 0 ether);
+        assertEq(iou.balanceOf(polling), 100 ether);
+        assertEq(gov.balanceOf(chief), 100 ether);
+
+        cold.doProxyFree(100 ether);
+        assertEq(gov.balanceOf(cold), 100 ether);
+        assertEq(iou.balanceOf(polling), 0 ether);
+        assertEq(gov.balanceOf(chief), 0 ether);
+    }
+
+    function test_voting() public {
+        cold.approveGov(proxy);
+        cold.doProxyLock(100 ether);
+        polling.warp(2 hours, 2);
+
+        address[] memory yays = new address[](1);
+        yays[0] = c1;
+        cold.doProxyVoteExec(yays);
+        assertEq(chief.approvals(c1), 100 ether);
+        assertEq(chief.approvals(c2), 0 ether);
+
+        address[] memory _yays = new address[](1);
+        _yays[0] = c2;
+        hot.doProxyVoteExec(_yays);
+        assertEq(chief.approvals(c1), 0 ether);
+        assertEq(chief.approvals(c2), 100 ether);
+
+        uint id = polling.createPoll(1, bytes32(1), 1, 1);
+        (, , , uint votesFor_, ) = polling.getPoll(id);
+        assertEq(votesFor_, 0 ether);
+        cold.doProxyVoteGov(id, true, byts);
+        (, , , uint _votesFor, ) = polling.getPoll(id);
+        assertEq(_votesFor, 100 ether);
+    }
+
+    function test_hot_free() public {
+        cold.approveGov(proxy);
+        assertEq(gov.balanceOf(cold), 100 ether);
+        assertEq(iou.balanceOf(polling), 0 ether);
+        assertEq(gov.balanceOf(chief), 0 ether);
+
+        cold.doProxyLock(100 ether);
+        assertEq(gov.balanceOf(cold), 0 ether);
+        assertEq(iou.balanceOf(polling), 100 ether);
+        assertEq(gov.balanceOf(chief), 100 ether);
+
+        hot.doProxyFree(100 ether);
+        assertEq(gov.balanceOf(cold), 100 ether);
+        assertEq(iou.balanceOf(polling), 0 ether);
+        assertEq(gov.balanceOf(chief), 0 ether);
+    }
+
+    function testFail_no_proxy_approval() public {
+        cold.doProxyLock(100 ether);
+    }
+
+    function testFail_random_free() public {
+        cold.approveGov(proxy);
+        cold.doProxyLock(100 ether);
+        random.doProxyFree(100 ether);
+    }
+
+    function testFail_random_vote() public {
+        cold.approveGov(proxy);
+        cold.doProxyLock(100 ether);
+
+        address[] memory yays = new address[](1);
+        yays[0] = c1;
+        random.doProxyVoteExec(yays);
+    }
+}
